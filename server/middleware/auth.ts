3b52e1f2a1366d79b581df387f088985a8f9e3ab
fabiana Silva <fabiana.sylva99@gmail.com>
2026-08-14 10:45:23 -0300

    initial commit

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,6 @@
 
 import { NextFunction, Request, Response } from "express";
-import { Jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   try{
@@ -11,7 +11,7 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
     }
 
     const token = authHeader.split(" ")[1];
-    const decoded = Jwt.verify(token, process.env.JWT_SECRET as string) as {id: string}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {id: string}
 
     req.user = {id: decoded.id}
     next()
